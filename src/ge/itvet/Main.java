2edb2a4c1b9c9f7c3944bb1ba087bc0bddaf7182
flash <flashdato@gmail.com>
2020-07-12 23:00:47 +0400

    ss

--- src/ge/itvet/Main.java
+++ src/ge/itvet/Main.java
@@ -4,14 +4,6 @@ import javax.swing.*;
 
 class Main {
     public static void main(String[] arg) {
-        MyPanel myPanel = new MyPanel();
-        JFrame jframe = new JFrame();
-        jframe.add(myPanel);
-        jframe.pack();
-        jframe.setTitle("TicTacToe");
-        jframe.doLayout();
-        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jframe.setResizable(true);
-        jframe.setVisible(true);
+        SwingUtilities.invokeLater(() -> Minesweeper1.run(10));
     }
 }
